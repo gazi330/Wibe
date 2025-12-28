78d264c04d650e31cc161d943a6afbaa56b60814
gazi330 <superbey574@gmail.com>
2025-12-28 11:24:19 +0300

    v0.3
    Dosya düzeni yapıldı

--- Proje/assets/js/config.js
+++ Proje/assets/js/config.js
@@ -5,6 +5,6 @@ window.CONFIG = {
     SUPABASE_URL: "https://tzuefqutxnqlnuuodrsf.supabase.co",
     SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6dWVmcXV0eG5xbG51dW9kcnNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY3NjkxODgsImV4cCI6MjA4MjM0NTE4OH0.XrJAcay7ELHD-HDnBQ9tDqyoK838w0t23yHopALdgeM",
     YOUTUBE_API_KEY: "AIzaSyAm0CjFKZ-Rheauy0Qvp5xCyfsjYYEkMcc", // Lütfen geçerli bir YouTube API anahtarı girin
-    GEMINI_API_KEY: "AIzaSyCxQEa8dwXJWB6aFkwhi9B_W2_b_fQqX3U", // Lütfen buraya Gemini API anahtarını girin
+    GEMINI_API_KEY: "AIzaSyDWkwRXTSVRiMUx_LwTmOFFb2olvz_MRPM", // Lütfen buraya Gemini API anahtarını girin
     GOOGLE_CLIENT_ID: "941723538889-o4s8062gv8oshtnmidjppbvvk0j9g0vj.apps.googleusercontent.com" // Supabase Dashboard'a eklenmeli
 };
